c9487a685ad327e2fb7390f77ea421a0b46a3900
Nicholas Fleichhauer <unobtainiumrock@gmail.com>
2024-08-30 13:22:30 -0700

    Fix route naming, convert build scripts over to yarn from npm

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 const jobRoutes = require('./jobs');
-const authRoutes = require('./authRoutes')
+const authRoutes = require('./auth')
 
 router.use('/api/jobs', jobRoutes);
 router.use('/api/auth', authRoutes);
