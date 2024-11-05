583667502188147b6afe346260254df45f9dce2d
Nicholas Fleichhauer <unobtainiumrock@gmail.com>
2024-11-04 19:09:09 -0800

    Refactor and clean up server index file. Add in CORS logic and more middlewares. Refactored the db connection logic flow

--- backend/config/index.js
+++ backend/config/index.js
@@ -1,31 +1,26 @@
 const passport = require('passport');
-const LocalStrategy = require('./passport');
+const LocalStrategy = require('./passport-local');
 const GoogleStrategy = require('./passport-google');
 const db = require('../models');
 
-passport.serializeUser((user, done) => {
-	console.log('=== serialize ... called ===')
-	console.log(user) // the whole raw user object!
-	console.log('---------')
-	done(null, { _id: user._id })
-});
-
-passport.deserializeUser((id, done) => {
-	console.log('Deserialize ... called')
-	db.User.findOne(
-		{ _id: id },
-		'firstName lastName photos local.username',
-		(err, user) => {
-			console.log('======= DESERIALIZE USER CALLED ======')
-			console.log(user)
-			console.log('--------------')
-			done(null, user)
-		}
-	)
-});
-
-// ==== Register Strategies ====
+// Register Strategies
 passport.use(LocalStrategy);
 passport.use(GoogleStrategy);
+
+
+// Serialize user for the session
+passport.serializeUser((user, done) => {
+  done(null, user._id);
+});
+
+// Deserialize user from the session
+passport.deserializeUser(async function(id, done) {
+  try {
+    const user = await db.User.findById(id);
+    done(null, user);
+  } catch (err) {
+    done(err, null);
+  }
+});
 
 module.exports = passport;
