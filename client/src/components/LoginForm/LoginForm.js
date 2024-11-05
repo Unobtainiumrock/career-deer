6adf7ae638cc53dbc2c14e60e0ea43e623230058
Nicholas Fleichhauer <unobtainiumrock@gmail.com>
2024-11-04 19:12:45 -0800

    semi-modernize react components for login to include form validation and validation schemas using Yup.js

--- client/src/components/LoginForm/LoginForm.js
+++ client/src/components/LoginForm/LoginForm.js
@@ -1,174 +1,107 @@
-// import React from 'react';
-// import { useForm, Controller } from 'react-hook-form';
-// import { TextField, Button, Box, Typography } from '@mui/material';
-// import { validate } from './validate';
-
-
-// const FormStyle = {
-//   background: '#fff',
-//   borderRadius: '15px',
-//   padding: '15px',
-//   boxShadow: '0px 0px 1px #5B5B5B'
-// }
-
-// // const LoginForm = ({ dispatch }) => {
-// //   const { control, handleSubmit, formState: { errors, isSubmitting }, reset } = useForm({
-// //     defaultValues: {
-// //       email: '',
-// //       password: ''
-// //     },
-// //     mode: 'onChange'
-// //   });
-
-// const renderTextField = ({
-//   input,
-//   label,
-//   meta: { touched, error },
-//   ...custom
-// }) => (
-//     <TextField
-//       hintText={label}
-//       floatingLabelText={label}
-//       errorText={touched && error && <span>{error}</span>}
-//       {...input}
-//       {...custom}
-//     />
-//   )
-
-// let LoginForm = ({ handleSubmit, pristine, submitting, errorMessage, auth }) => {
-//   return (
-//     <form style={FormStyle} className="text-center" onSubmit={handleSubmit}>
-//       <div>
-//         <Field className="text-input" name="email" component={renderTextField} type="email" label="Email" />
-//       </div>
-//       <div>
-//         <Field className="text-input" name="password" component={renderTextField} type="password" label="Password" />
-//       </div>
-//       <div className="mt-3">
-//         <h6>{errorMessage}</h6>
-//         <div className="text-right">
-//         <Button variant="contained" color="primary" className="roboto login-btn btn" type="submit" disabled={pristine || submitting}>
-//           Login &nbsp; <i className="fas fa-sign-in-alt"></i>
-//         </Button>&nbsp;&nbsp;
-//         {/* <Button onClick={auth} className="roboto login-btn btn btn-light"> */}
-//         Login with <img className="ml-1" height="20px" src="/imgs/icons/google-logo.svg" alt="google logo"/>
-//         {/* </Button> */}
-//         </div>
-//       </div>
-//     </form>
-//   )
-// };
-
-// LoginForm = reduxForm({
-//   // a unique name for the form
-//   form: 'login',
-//   validate,
-//   // clears the form after submission
-//   onSubmitSuccess: (result, dispatch) => dispatch(reset('login'))
-// })(LoginForm);
-
-// // Inside this file, we wrapped our component inside the imported 'reduxForm' function
-// // We can think of reduxForm(), from redux-form, behaving similar to connect() from react-redux in
-// // terms of connecting a component to communicate with the store 
-// export default LoginForm;
-
 import React from 'react';
 import { useForm, Controller } from 'react-hook-form';
-import TextField from '@mui/material/TextField'; // Ensure you're using @mui/material
-import Button from '@mui/material/Button';
-import { FormHelperText } from '@mui/material';
-
+import { TextField, Button, CircularProgress, Box, FormHelperText } from '@mui/material';
+import PropTypes from 'prop-types';
+import { yupResolver } from '@hookform/resolvers/yup';
+import { loginValidationSchema } from './validate';
 const FormStyle = {
-  // Define your form styles here or import them if defined elsewhere
+  background: '#fff',
+  borderRadius: '15px',
+  marginTop: '10px',
+  padding: '20px',
+  boxShadow: '0px 0px 10px rgba(0, 0, 0, 0.1)'
 };
 
-const LoginForm = ({ onSubmit, errorMessage, auth }) => {
+const LoginForm = ({ onSubmit, errorMessage }) => {
   const {
     handleSubmit,
     control,
     formState: { errors, isSubmitting, isDirty },
-    reset,
-  } = useForm();
+    reset
+  } = useForm({
+    defaultValues: {
+      email: '',
+      password: ''
+    },
+    mode: 'onChange',
+    resolver: yupResolver(loginValidationSchema)
+  });
 
   const onSubmitForm = (data) => {
-    // Call the onSubmit prop passed from the parent component
     onSubmit(data);
-    // Reset the form after submission
     reset();
   };
 
   return (
-    <form style={FormStyle} className="text-center" onSubmit={handleSubmit(onSubmitForm)}>
-      <div>
+    <form style={FormStyle} onSubmit={handleSubmit(onSubmitForm)}>
+      <Box mb={2}>
         <Controller
           name="email"
           control={control}
-          defaultValue=""
-          rules={{
-            required: 'Email is required',
-            pattern: {
-              value: /^[^@ ]+@[^@ ]+\.[^@ .]{2,}$/,
-              message: 'Email is not valid',
-            },
-          }}
           render={({ field }) => (
             <TextField
               {...field}
-              className="text-input"
               label="Email"
               variant="outlined"
+              fullWidth
               error={!!errors.email}
               helperText={errors.email ? errors.email.message : ''}
-              fullWidth
-              margin="normal"
             />
           )}
         />
-      </div>
-      <div>
+      </Box>
+      <Box mb={2}>
         <Controller
           name="password"
           control={control}
-          defaultValue=""
-          rules={{ required: 'Password is required' }}
           render={({ field }) => (
             <TextField
               {...field}
-              className="text-input"
               label="Password"
               type="password"
               variant="outlined"
+              fullWidth
               error={!!errors.password}
               helperText={errors.password ? errors.password.message : ''}
-              fullWidth
-              margin="normal"
             />
           )}
         />
-      </div>
-      <div className="mt-3">
-        {errorMessage && <FormHelperText error>{errorMessage}</FormHelperText>}
-        <div className="text-right">
-          <Button
-            variant="contained"
-            color="primary"
-            className="roboto login-btn btn"
-            type="submit"
-            disabled={!isDirty || isSubmitting}
-          >
-            Login &nbsp; <i className="fas fa-sign-in-alt"></i>
-          </Button>
-          &nbsp;&nbsp;
-          {/* If you have a function to handle Google login, uncomment the button below */}
-          {/* 
-          <Button onClick={auth} className="roboto login-btn btn btn-light">
-            Login with <img className="ml-1" height="20px" src="/imgs/icons/google-logo.svg" alt="google logo" />
-          </Button>
-          */}
-        </div>
-      </div>
+      </Box>
+      {errorMessage && (
+        <Box mb={2}>
+          <FormHelperText error>{errorMessage}</FormHelperText>
+        </Box>
+      )}
+      <Box display="flex" justifyContent="space-between" alignItems="center">
+        <Button
+          variant="contained"
+          color="primary"
+          type="submit"
+          disabled={!isDirty || isSubmitting}
+          startIcon={isSubmitting && <CircularProgress size={20} />}
+        >
+          {isSubmitting ? 'Logging In...' : 'Login'}
+        </Button>
+        <Button
+          variant="outlined"
+          color="secondary"
+          onClick={() => window.location.href = '/api/auth/google'}
+          disabled={isSubmitting}
+        >
+          Login with Google
+        </Button>
+      </Box>
     </form>
   );
+};
+
+LoginForm.propTypes = {
+  onSubmit: PropTypes.func.isRequired,
+  errorMessage: PropTypes.string
+};
+
+LoginForm.defaultProps = {
+  errorMessage: ''
 };
 
 export default LoginForm;
--- client/src/components/LoginForm/validate.js
+++ client/src/components/LoginForm/validate.js
@@ -1,12 +1,10 @@
-export const validate = values => {
-  const errors = {};
-  if (!values.email) {
-    errors.email = 'You must enter an email'
-  } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
-    errors.email = 'Please enter a valid email address'
-  };
-  if (!values.password) {
-    errors.password = 'A password is required'
-  };
-  return errors;
-};
+import * as Yup from 'yup';
+
+export const loginValidationSchema = Yup.object().shape({
+  email: Yup.string()
+    .required('MissingEmailError')
+    .email('InvalidCredentialsError'),
+  password: Yup.string()
+    .required('MissingPasswordError')
+    .min(8, 'InvalidCredentialsError')
+});
